3a1236ab1b026b396e486615579a713a8c4920e1
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-25 08:30:59 -0700

    getAll route adjusted, test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,52 +11,41 @@ describe('demo routes', () => {
 
   it('retrieves studios via GET', async () => {
     
-    const Studios = await Studio.bulkCreate([
+    const studioData = await Studio.bulkCreate([
       {
-        id: '1',
         name: 'Fox',
         city: 'Hollywood',
         state: 'California',
         country: 'United States'
       },
       {
-        id: '2',
         name: 'Paramount',
         city: 'Hollywood',
         state: 'California',
         country: 'United States'
       },
       {
-        id: '3',
         name: 'Kinet',
         city: 'New York',
         state: 'New York',
         country: 'United States'
       }
     ]);
-    
+    console.log('hello', studioData[0].dataValues.createdAt);
     const res = await request(app).get('/api/v1/studios');
-    expect(res.body).toEqual({ Studios: [{
-      id: '1',
-      name: 'Fox',
-      city: 'Hollywood',
-      state: 'California',
-      country: 'United States'
-    },
-    {
-      id: '2',
-      name: 'Paramount',
-      city: 'Hollywood',
-      state: 'California',
-      country: 'United States'
-    },
-    {
-      id: '3',
-      name: 'Kinet',
-      city: 'New York',
-      state: 'New York',
-      country: 'United States'
-    }], updatedAt: Studios.updatedAt.toISOString(), createdAt: Studios.createdAt.toISOString() });
+    expect(res.body).toEqual([{ 
+      ...studioData[0].dataValues,
+      updatedAt: studioData[0].dataValues.updatedAt.toISOString(),
+      createdAt: studioData[0].dataValues.createdAt.toISOString() },
+      
+    { ...studioData[1].dataValues,
+      updatedAt: studioData[1].dataValues.updatedAt.toISOString(),
+      createdAt: studioData[1].dataValues.createdAt.toISOString() },
+
+    { ...studioData[2].dataValues,
+      updatedAt: studioData[2].dataValues.updatedAt.toISOString(),
+      createdAt: studioData[2].dataValues.createdAt.toISOString(),
+    }]);
 
   });
 });
