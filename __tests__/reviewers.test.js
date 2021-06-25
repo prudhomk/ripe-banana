aba9078c98b2f21d6e8fc88cdf1c06b51e14fcd5
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-25 15:02:45 -0700

    create failing GET all reviewers test

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -23,6 +23,38 @@ describe('reviewers routes', () => {
       createdAt: expect.any(String)
     });
   });
+
+  it('retrieves all reviewers via GET', async () => {
+    const reviewerData = await Reviewer.bulkCreate([
+      {
+        name: 'Roper Egger',
+        company: 'Randy Ebbs' 
+      },
+      {
+        name: 'Armando White',
+        company: 'Troll Inc'
+      },
+      {
+        name: 'Jiminy Glick',
+        company: 'Hollywood Blvd'
+      }
+    ]);
+
+    const res = await request(app).get('/api/v1/reviewers');
+    expect(res.body).toEqual([{
+      ...reviewerData[0].dataValues,
+      updatedAt: reviewerData[0].dataValues.updatedAt.toISOString(),
+      createdAt: reviewerData[0].dataValues.createdAt.toISOString() },
+    {
+      ...reviewerData[1].dataValues,
+      updatedAt: reviewerData[1].dataValues.updatedAt.toISOString(),
+      createdAt: reviewerData[1].dataValues.createdAt.toISOString() },
+    {
+      ...reviewerData[2].dataValues,
+      updatedAt: reviewerData[2].dataValues.updatedAt.toISOString(),
+      createdAt: reviewerData[2].dataValues.createdAt.toISOString() }
+    ]);
+  });
 });
 
 
