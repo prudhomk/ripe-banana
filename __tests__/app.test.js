59791b8258b0a857242b3440b9d70abdd2371131
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-24 17:02:51 -0700

    wrote studio model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,21 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
+import sequelize from '../lib/utils/sequelize.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+
 describe('demo routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    return sequelize.sync({ force: true });
   });
 
   it('retrieves studios via GET', async () => {
+    
+    const studio = await Studio.create({
+      id: '1',
+      name: 'Fox'
+    });
+    
     const res = await request(app).get('/studios');
-
     expect(res).toEqual([{
       id: expect.any(Number),
       name: expect.any(String)
