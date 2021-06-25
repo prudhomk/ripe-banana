4305df3bd018d33d5b510b6636e14ddcc56318e2
culi <culitif@tuta.io>
2021-06-25 11:31:49 -0700

    create failing Post actors test

---
+++ __tests__/actors.test.js
@@ -0,0 +1,30 @@
+import sequelize from '../lib/utils/db.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Actor from '../lib/models/Actor.js';
+
+describe('actors routes', () => {
+  beforeEach(() => {
+    return sequelize.sync({ force: true });
+  });
+
+  const martinShort = {
+    name: 'Martin Short',
+    dob: new Date('1950-03-26'),
+    pob: 'Hamilton, Ontario, Canada'
+  };
+
+  it('POST route for actors', async () => {
+    const res = await request(app) 
+      .post('/api/v1/actors')
+      .send(martinShort)
+    ;
+
+    expect(res.body).toEqual({
+      id: expect.any(Number),
+      ...martinShort,
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
+});
