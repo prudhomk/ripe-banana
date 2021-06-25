9adba1a8cba42a2b2b9ec82325fbdb9258b308ff
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-25 14:40:57 -0700

    create failing POST test

---
+++ __tests__/reviewers.test.js
@@ -0,0 +1,27 @@
+import sequelize from '../lib/utils/db.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('reviewers routes', () => {
+  beforeEach(() => {
+    return sequelize.sync({ force: true });
+  });
+
+  it('adds a reviewer via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/reviewers')
+      .send({
+        name: 'Roger Ebert',
+        company: 'Robert Eger'
+      });
+    expect(res.body).toEqual({
+      id: 1,
+      name: 'Roger Ebert',
+      company: 'Robert Eger',
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
+});
+
+
