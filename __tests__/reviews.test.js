613340d0c2c7de899fbec7d29b1061bbaafeac90
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-25 17:49:54 -0700

    create failing pOST test

---
+++ __tests__/reviews.test.js
@@ -0,0 +1,25 @@
+import sequelize from '../lib/utils/db.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('reviews routes', () => {
+  beforeEach(() => {
+    return sequelize.sync({ force: true });
+  });
+
+  it('creates reviews via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/reviews')
+      .send({
+        rating: 3,
+        review: 'I thought it was not that great but kind of good.'
+      });
+    expect(res.body).toEqual({
+      id: 1,
+      rating: 3,
+      review: 'I thought it was not that great but kind of good.',
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
+});
