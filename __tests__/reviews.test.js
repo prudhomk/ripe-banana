d6ba76538d6ad939b5fbeb27dc9bd745e40a3da4
culi <culitif@tuta.io>
2021-06-29 09:33:20 -0700

    make failing delete review test

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -35,21 +35,6 @@ describe('reviews routes', () => {
     });
   });
 
-  it('gets a review by id via GET', async () => {
-    const review = await Review.create(review1);
-
-    const res = await request(app)
-      .get(`/api/v1/reviews/${review.id}`)
-    ;
-
-    expect(res.body).toEqual({
-      ...review1,
-      id: 1,
-      updatedAt: expect.any(String),
-      createdAt: expect.any(String)
-    });
-  });
-
   it('gets all reviews via GET', async () => {
     await Review.create(review1);
     await Review.create(review2);
@@ -70,5 +55,29 @@ describe('reviews routes', () => {
         createdAt: expect.any(String)
       }
     ]);
+  });
+
+  it('delete review by id via DELETE', async () => {
+    const review = await Review.create(review1);
+
+    const res = await request(app)
+      .delete(`/api/v1/reviews/${review.id}`);
+    const res2 = await request(app)
+      .get('/api/v1/reviews');
+
+    expect(res.body).toEqual({
+      ...review1,
+      id: 1,
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+    expect(res2.body).not.toEqual(expect.arrayContaining([
+      {
+        ...review1,
+        id: 1,
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String)
+      }
+    ]));
   });
 });
