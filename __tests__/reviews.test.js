e0947ffe1834057729f9c41035d161bad2e0ea67
culi <culitif@tuta.io>
2021-06-29 11:07:47 -0700

    modify reviews tests for foreign keys

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -11,58 +11,52 @@ describe('reviews routes', () => {
 
   const review1 = {
     rating: 3,
-    review: 'I thought it was not that great but kind of good.'
+    review: 'I thought it was not that great but kind of good.',
   };
 
   const review2 = {
     rating: 5,
-    review: 'it sucked'
+    review: 'it sucked',
   };
 
   it('creates reviews via POST', async () => {
     const res = await request(app)
       .post('/api/v1/reviews')
-      .send({
-        rating: 3,
-        review: 'I thought it was not that great but kind of good.'
-      });
+      .send(review1);
     
     expect(res.body).toEqual({
+      ...review1,
       id: 1,
-      rating: 3,
-      review: 'I thought it was not that great but kind of good.',
       updatedAt: expect.any(String),
       createdAt: expect.any(String)
     });
   });
 
   it('gets all reviews via GET', async () => {
-    await Review.create(review1);
-    await Review.create(review2);
+    await Film.create({ title: 'The Room', studio: '1', released: 2003 });
+    await Film.create({ title: 'Clotheslines', studio: '2', released: 1981 });
 
-    Film.bulkCreate([
-      { title: 'The Room', studio: '1', released: 2003 },
-      { title: 'Clotheslines', studio: '2', released: 1981 }
-    ]);
+    await Review.create({ ...review1, film: 1, reviewer: '1' });
+    await Review.create({ ...review2, film: 2, reviewer: '1' });
 
     const res = await request(app).get('/api/v1/reviews');
 
-    expect(res.body).toEqual([
+    expect(res.body).toEqual(expect.arrayContaining([
       {
-        ...review1,
-        film: [{ id: '1', title: 'The Room' }],
-        id: 1,
+        ...review2,
+        film: { id: '2', title: 'Clotheslines' },
+        id: 2,
         updatedAt: expect.any(String),
         createdAt: expect.any(String)
       },
       {
-        ...review2,
-        film: [{ id: '2', title: 'Clotheslines' }],
-        id: 2,
+        ...review1,
+        film: { id: '1', title: 'The Room' },
+        id: 1,
         updatedAt: expect.any(String),
         createdAt: expect.any(String)
       }
-    ]);
+    ]));
   });
 
   it('delete review by id via DELETE', async () => {
