cb83f485c2cc1724f03a6b79ddc574895afa5ac8
culi <culitif@tuta.io>
2021-06-29 09:23:20 -0700

    create test for get review by id

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -35,11 +35,26 @@ describe('reviews routes', () => {
     });
   });
 
+  it('gets a review by id via GET', async () => {
+    const review = await Review.create(review1);
+
+    const res = await request(app)
+      .get(`/api/v1/reviews/${review.id}`)
+    ;
+
+    expect(res).toEqual({
+      ...review1,
+      id: 1,
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
+
   it('gets all reviews via GET', async () => {
     await Review.create(review1);
     await Review.create(review2);
 
-    const res = await request(app).get('/api/v1/review');
+    const res = await request(app).get('/api/v1/reviews');
 
     expect(res.body).toEqual([
       {
